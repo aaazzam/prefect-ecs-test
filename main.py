3b51b388694d2cd306cbcca86b3085f328fb7375
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-06-13 12:36:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
+from prefect import flow
+
+
+@flow
+def hello_world():
     print("Hello from prefect-ecs-test!")
 
 
 if __name__ == "__main__":
-    main()
+    flow.from_source(
+        source="https://github.com/aaazzam/prefect-ecs-test.git",
+        entrypoint="main.py:hello_world",
+    ).deploy(
+        name="hello-world",
+        work_pool_name="noice",
+    )
